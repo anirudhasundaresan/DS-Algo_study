2b1d38237e85028735dd8c3995b44766532e72a8
pink_floyd <12726389+anirudhasundaresan@users.noreply.github.com>
2018-06-11 14:27:07 -0400

    Add files via upload

---
+++ selection_sort.py
@@ -0,0 +1,18 @@
+# To implement selection sort. Same as bubble sort but with more exchanges and same O(N2).
+# Iterative procedure
+
+def selection_sort(alist):
+    for i in range(len(alist)-1):
+        max = -np.inf
+        pos = 0
+        for j in range(len(alist)-i):
+            if alist[j] > max:
+                max = alist[j]
+                pos = j
+        print(alist)
+        alist[pos], alist[len(alist)-i-1] = alist[len(alist)-i-1], alist[pos]
+    return alist
+
+alist = [54,26,93,17,77,31,44,55,20]
+import numpy as np
+print(selection_sort(alist))
